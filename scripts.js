1d4346d4d9c55946761c305e08570ffe2554b5e2
felipehiranojp <felipehiranojp@yahoo.com.br>
2024-03-06 12:26:30 -0300

    06-03

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-function calcular(){
+function calcular(pagina){
+    //mudar para a página seguinte
+    window.location="quiz"+pagina+".html"
 
     // tranforma as variaveis sessionStorage em variaveis e adiciona +1
     var camp = parseInt(sessionStorage.getItem("campestre")); camp =camp+1;
@@ -11,7 +13,7 @@ function calcular(){
     var vint = parseInt(sessionStorage.getItem("vintage")); vint =vint+1;
     
 
-    var check1 = document.getElementById("checkbox1");//atribui uma variaveis ao botao checkbox
+    var check1 = document.getElementById("checkbox1");//atribui uma variavel ao botao checkbox
     var check2 = document.getElementById("checkbox2");
     var check3 = document.getElementById("checkbox3");
     var check4 = document.getElementById("checkbox4");
@@ -30,8 +32,29 @@ function calcular(){
     if (check9.checked){var vint = sessionStorage.setItem("vintage", vint);};
 
 
+    
+
     var tela = document.getElementById('teste'); //escreve os valores das variaveis na tela de teste
     tela.innerHTML=( 'campestre= '+sessionStorage.getItem("campestre")+'<br>Classica = '+ sessionStorage.getItem("classica")+'<br>Escandinavo = '+ sessionStorage.getItem("escandinavo")+'<br>Industrial = '+ sessionStorage.getItem("industrial")+'<br>Minimalista = '+ sessionStorage.getItem("minimalista")+'<br>Moderna = '+ sessionStorage.getItem("moderna")+'<br>Retro = '+ sessionStorage.getItem("retro")+'<br>Vintage = '+ sessionStorage.getItem("vintage"));
+    }
+
+function resposta(){
+    var camp = parseInt(sessionStorage.getItem("campestre")); 
+    var clas = parseInt(sessionStorage.getItem("classica")); 
+    var esca = parseInt(sessionStorage.getItem("escandinavo"));
+    var indu = parseInt(sessionStorage.getItem("industrial")); 
+    var mini = parseInt(sessionStorage.getItem("minimalista")); 
+    var mode = parseInt(sessionStorage.getItem("moderna")); 
+    var retr = parseInt(sessionStorage.getItem("retro"));
+    var vint = parseInt(sessionStorage.getItem("vintage")); 
+    var tela2 = document.getElementById("cards2");
+    var grupo = [camp,clas,esca,indu,mini,mode,retr,vint];
+    var maior =grupo[0]
+    for (i=1;i<=8;i++){
+        if(maior < grupo[i]){
+            maior = grupo[i]
+        }
+        tela2.innerHTML=('o maior é: '+ );
+       
+    }}
     
-    
-}
